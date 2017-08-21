1fbba667dfa706b511373709a25c6b4cf6dd804c
riceDarling <2398300521@qq.com>
2017-08-21 18:04:43 +0800

    2017-08-21 18:04:32
    Signed-off-by: riceDarling <2398300521@qq.com>

--- WebRoot/account/lib/js/config.js
+++ WebRoot/account/lib/js/config.js
@@ -13,5 +13,6 @@ function formatDateTime(inputTime) {
     var second = date.getSeconds();  
     minute = minute < 10 ? ('0' + minute) : minute;    
     second = second < 10 ? ('0' + second) : second;   
-    return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;    
+    //return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;  
+    return y + '-' + m + '-' + d;
 };  
